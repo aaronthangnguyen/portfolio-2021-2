fd85a1a89c8e208e84561830a4937537bf0ca54d
Aaron Nguyen <me@aaronnguyen.dev>
2021-11-04 02:16:10 -0400

    Add education section

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,79 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Aaron Nguyen</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main></main>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+          Aaron{" "}
+          <a
+            className="p-2 text-blue-600 transition duration-300 ease-out hover:bg-red-600 hover:text-white"
+            href="https://www.linkedin.com/in/aaronthangnguyen/"
+            target="_blank"
+          >
+            Nguyen 🌋
           </a>
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+        <code className="p-3 mt-5 text-lg font-bold bg-gray-100 rounded-lg">
+          Full-stack Software Engineer
+        </code>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        <div className="flex flex-wrap items-stretch max-w-4xl mt-6 justify-evenly sm:w-full odd:after:w-96">
+          <div className="p-6 text-left transition duration-300 ease-in-out transform w-96 rounded-xl group hover:bg-yellow-50 hover:shadow-lg hover:translate-y-1 hover:scale-105">
+            <h3 className="mb-2 text-2xl font-bold group-hover:text-yellow-500">
+              University of Central Florida
+            </h3>
+            <ul className="text-lg text-gray-400 group-hover:text-yellow-400">
+              <li>Bachelor of Computer Science</li>
+              <li>
+                GPA: <span className="font-bold">4.0</span>
+              </li>
+              <li>2022 - 2024 | expected</li>
+            </ul>
+          </div>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+          <div className="p-6 text-left transition duration-300 ease-in-out transform w-96 rounded-xl group hover:bg-red-50 hover:shadow-lg hover:translate-y-1 hover:scale-105">
+            <h3 className="mb-2 text-2xl font-bold group-hover:text-red-500">
+              Valencia College
+            </h3>
+            <ul className="text-lg text-gray-400 group-hover:text-red-400">
+              <li>Associate of Arts</li>
+              <li>
+                GPA: <span className="font-bold">4.0</span>
+              </li>
+              <li>2021 - 2022 | transferred</li>
+            </ul>
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className="p-6 mt-3 text-left transition duration-300 ease-in-out transform rounded-xl w-96 group hover:bg-blue-50 hover:shadow-lg hover:translate-y-1 hover:scale-105">
+            <h3 className="mb-2 text-2xl font-bold group-hover:text-blue-500">
+              Ton Duc Thang University
+            </h3>
+            <ul className="text-lg text-gray-400 group-hover:text-blue-400">
+              <li>Bachelor of Laws</li>
+              <li>2014 - 2018 | graduated</li>
+            </ul>
+          </div>
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="flex items-center justify-center w-full h-24">
         <a
           className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/in/aaronthangnguyen/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          Aaron Nguyen © 2021
         </a>
       </footer>
     </div>
-  )
+  );
 }
